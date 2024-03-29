dd553e38d2d78606f45988dce785bf632fe59419
Sigui Kessé Emmanuel <contact@sigui.ci>
2024-03-29 18:15:34 +0100

    :white_check_mark: Test serve() and run() functions

--- spec/index.test.ts
+++ spec/index.test.ts
@@ -1,4 +1,5 @@
 import { expect, it } from "bun:test";
+import { run, serve } from "boss.sh";
 import hello_php from "./hello.php";
 import hello_v from "./hello.vsh";
 
@@ -8,4 +9,19 @@ it("should import PHP file", () => {
 
 it("should import V file", () => {
   expect(hello_v).toBe("Hello V!");
+});
+
+it("can run V file", () => {
+  expect(run(`${import.meta.dir}/hello.vsh`, "v run")).toBeEmpty();
+});
+
+it("can serve PHP file", () => {
+  const server = Bun.serve({
+    async fetch(request: Request, server) {
+      const response = await serve(`${import.meta.dir}/hello.php`, "php");
+      expect(response).toBeInstanceOf(Response);
+    },
+  });
+
+  console.log(`Server running at http://localhost:${server.port}`);
 });
